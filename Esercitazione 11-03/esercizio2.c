3cec0d0816b8ac7918c0b417c5190f15f2ba697a
CatalinPast <pastor.catalin@gmail.com>
2024-03-11 17:37:13 +0100

    11-03-2024

--- Esercitazione 11-03/esercizio2.c
+++ Esercitazione 11-03/esercizio2.c
@@ -4,11 +4,9 @@ int a=4,b=3,c=3;
 float f1=1.1,f2=2.2,f3=3.3;
 unsigned char c1='a',c2='b',c3='c';
 
-int main (  )
+int main ( void )
 {
-    a+=b++;
-    f1+=f2++;
-    c1=++c2;
+    
 
     printf("Row1 : %d %d %d \n",a,b,c);
     printf("Row2 : %f %f %f \n",f1,f2,f3);
---
+++ Esercitazione 11-03/esercizio3.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main( void )
+{
+    int a = 15;
+    int b = -8;
+    //printf("Var : /t %x",a);
+    //printf("Var : %x",a);
+    printf(" Var : %d ",sizeof a);
+    //binario e ottale pure
+    //vedi che succede tipo char il max è 255 allora vedi che succede se aggiungi 1 ed esce da intervallo e tutto quanto v
+    //linux considera c come genitore infatti man printf ce puoi usare i comandi su shell!!!!!!!!!!
+    //gestione memoria e tutto poi in piu esercizi e coes vediti su internet
+    //limits.h contiene limiti di tutti tipi
+    //sizeof come rappresenta in memoria
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
